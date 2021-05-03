1dc41a8712bb3fc12d428185558a92ece1132b3b
Antonio Tonković <Antonio.Tonkovic@fer.hr>
2021-05-03 21:48:25 +0200

    Dodan build folder

---
+++ client/build/precache-manifest.ab4bf2dc71c74ef0a61a1a6e0179e68c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55bb489ea56aebd0d0ca6eaa425fdcf7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d55561489a1430f896b6",
+    "url": "/static/js/2.350c2809.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/static/js/2.350c2809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fabca7d374e2d41cd1e3",
+    "url": "/static/js/main.aec95fa1.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  },
+  {
+    "revision": "180f90688a36f09afa935563f52704bd",
+    "url": "/static/media/adminCategoriesImage.180f9068.jpg"
+  },
+  {
+    "revision": "41e6e393678d83d905b390956225d508",
+    "url": "/static/media/adminCountiesImage.41e6e393.jpg"
+  },
+  {
+    "revision": "71e18201ce4c0273352e6239b8df0598",
+    "url": "/static/media/adminPostOfficesImage.71e18201.jpg"
+  },
+  {
+    "revision": "42d8ab521ed58adf507398b8ca02459a",
+    "url": "/static/media/adminUnitsImage.42d8ab52.jpg"
+  },
+  {
+    "revision": "abfc0f1c559d6eb0a6544146372c0239",
+    "url": "/static/media/farmersImage.abfc0f1c.jpg"
+  },
+  {
+    "revision": "8ce3de0ab74a6c50c268be1438f9e04c",
+    "url": "/static/media/homeHeroImage.8ce3de0a.jpg"
+  },
+  {
+    "revision": "64d62472dfcc7bd6bac6f9979d3e9898",
+    "url": "/static/media/homeSubHeroImage.64d62472.jpg"
+  },
+  {
+    "revision": "e70c23adb0ab9c1bf4b7d85ced2abf77",
+    "url": "/static/media/loginImage.e70c23ad.jpg"
+  },
+  {
+    "revision": "0435bdbaed027125679ad0b528fd038e",
+    "url": "/static/media/productsImage.0435bdba.jpg"
+  },
+  {
+    "revision": "52e2f8bdb859669944dbf3b915d2459e",
+    "url": "/static/media/registerImage.52e2f8bd.jpg"
+  }
+]);
