669168c635020deff0692ed7015818ef28634b3a
Antonio Tonković <Antonio.Tonkovic@fer.hr>
2021-05-03 21:44:07 +0200

    Promijenjen path do build foldera

--- server/app.js
+++ server/app.js
@@ -42,12 +42,10 @@ app.use("/api/shopping_cart_page", require("./routes/api/shopping_cart_page"));
 
 // If in production, get files from the build folder
 if (ENV === "production") {
-	app.use(express.static("../client/build"));
+	app.use(express.static(path.join(__dirname, "/../client/build")));
 
 	app.get("*", (req, res) => {
-		res.sendFile(
-			path.join(__dirname, "..", "client", "build", "index.html")
-		);
+		res.sendFile(path.join(__dirname, "/../client/build/index.html"));
 	});
 }
 
